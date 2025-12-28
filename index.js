6c145d719c81f8f28c47864e914b8825c8c3a943
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2025-12-28 11:00:01 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ res.redirect('/home')})
 app.get('/favicon.ico', (req, res) => {
 res.redirect('https://github.com/DevZyxlJs.png')})
 app.use('/home', express.static(path.join(__dirname, 'src', 'index.html')))
+app.use('/test', express.static(path.join(__dirname, 'src', 'commands.js')))
 
 module.exports = app
